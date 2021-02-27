4dad87ea01ca441dfaadcdd50cc9b9dcf4ba9694
jeremydrn <60497084+jeremydrn@users.noreply.github.com>
2021-02-27 10:57:22 -0500

    Set up window into backend

--- index.ts
+++ index.ts
@@ -1,27 +1,31 @@
+import { Module } from "@nestjs/common";
 import "reflect-metadata"; 
 import {Connection, createConnection, getManager} from "typeorm";
 import {Company, Employee, Login} from "./entity/databaseStarter.js";
 
+var connection;
+
+export async function connect(){
 createConnection().then(async connection => { 
-
     
-    const testCompany = new Company();
-    testCompany.id = 1; testCompany.name = "Totally Real Company Inc.";
-
-    const testUser = new Employee();
-    testUser.employeeID = 1; testUser.firstName = "Test"; testUser.lastName = "User"; 
-    testUser.positionTitle = "A totally real job"; testUser.startDate = "February 29, 2021"; 
-    testUser.managerID = 1; testUser.companyID = 1;
-
-    const testLogin = new Login();
-    testLogin.email = "myemail@address.com"; testLogin.pswd = "password"; 
-    testLogin.empID = 1; testLogin.companyID = 1;
-
     const emps = connection.manager.getRepository(Employee);
     const comps = connection.manager.getRepository(Company);
     const logs = connection.manager.getRepository(Login);
 
     console.log(await emps.count({companyID : 1}));
 }).catch(error => console.log(error));
+}
 
+export function connected(){
+    return typeof connection !== 'undefined';
+}
 
+export function getEmployeeRepository(){
+    return connection.getRepository(Employee);
+}
+export function getCompanyRepository(){
+    return connection.getRepository(Company);
+}
+export function getLoginRepository(){
+    return connection.getRepository(Login);
+}
