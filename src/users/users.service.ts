7d16f2363064fdfa032991cfcb216ad9c167ff11
Dmitry Ilin <dimailinemail@gmail.com>
2021-03-01 21:26:16 -0500

    removed some unnecessary lines

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -31,7 +31,6 @@ export class UsersService {
     async findOne(username: string): Promise<User | undefined> {
         // return this.users.find(user => user.username === username);
         let user = this.usersRepository.findOne({ where: { email: username } });
-        console.log(user);
         return user;
     }
 }
