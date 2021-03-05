81b1d2a059d7a597bd8f9f64878ca236b5be031d
jeremydrn <60497084+jeremydrn@users.noreply.github.com>
2021-03-05 15:04:36 -0500

    Updated import references to typeorm entities

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Users } from './users.entity';
-import { Login } from './login.entity';
+import { Users } from 'src/entity/Users';
+import { Login } from 'src/entity/Login';
 //Change to a real interface
 export type User = any;
 
