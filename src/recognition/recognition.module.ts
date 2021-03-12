4b14336e4cc232f3fca150abf6c2109aad6cd123
matt-weiner <mweiner@umass.edu>
2021-03-12 18:26:07 -0500

    Updated Recognition Module

--- src/recognition/recognition.module.ts
+++ src/recognition/recognition.module.ts
@@ -3,10 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { RecognitionController } from './recognition.controller';
 import { RecognitionService } from './recognition.service';
 import {Recognition} from '../entity/recognition.entity'
+import { Users } from 'src/entity/users.entity';
+import { Tag } from 'src/entity/tag.entity';
+import { Company } from 'src/entity/company.entity';
 @Module({
-  imports: [ TypeOrmModule.forFeature([Recognition])],
+  imports: [ TypeOrmModule.forFeature([Recognition, Users, Tag, Company])],
   controllers: [ RecognitionController],
   providers: [ RecognitionService],
-
+  exports: [RecognitionService]
 })
 export class RecognitionModule {}
