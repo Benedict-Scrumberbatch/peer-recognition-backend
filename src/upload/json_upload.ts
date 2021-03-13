e5d5b28b9a60f4f0931cff2520e80ee73b88c232
William Mironchuk <wmironchuk@umass.edu>
2021-03-13 12:43:00 -0500

    Temp bug fix The function I need to run is confusing to implement.  I need to make sure it works

--- src/upload/json_upload.ts
+++ src/upload/json_upload.ts
@@ -2,3 +2,7 @@ import { getManager } from 'typeorm';
 import { Users } from '../entity/users.entity';
 
 const entityManager = getManager();
+
+function newUser(object): any {
+	// Given a generic user object, make a new instance of users
+}
