7be1a4b4e7d7d4e03307084e6fb53c7f3f0518ba
Dmitry Ilin <dimailinemail@gmail.com>
2021-03-13 15:40:20 -0500

    Removed unnecessary imports

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,9 +1,6 @@
 import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common';
-import { Roles } from 'src/roles/roles.decorator';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { UsersService } from './users.service';
-import { Role } from '../roles/role.enum';
-import { RolesGuard } from '../roles/roles.guard';
 
 @Controller('users')
 export class UsersController {
