0c3e5b8c04dc19b9934f748721e5459525c172fa
jeremydrn <60497084+jeremydrn@users.noreply.github.com>
2021-03-15 13:26:16 -0400

    Implemented insert get delete for company value tags

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,14 @@ import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import {RecognitionModule} from './recognition/recognition.module'
+import {TagModule} from './tag/tag.module'
 import { Users } from './entity/users.entity';
 import { Tag } from './entity/tag.entity';
 import { Company } from './entity/company.entity';
 import { RecognitionService } from './recognition/recognition.service';
 
 @Module({
-  imports: [AuthModule, UsersModule, RecognitionModule, TypeOrmModule.forRoot()],
+  imports: [AuthModule, UsersModule, RecognitionModule, TagModule, TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 
