bffd85e12a94c662471aa431a1031f83e8bee385
matt-weiner <mweiner@umass.edu>
2021-04-07 13:48:02 -0400

    added env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,27 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CompanyModule } from './company/company.module';
 import { RecognitionModule } from './recognition/recognition.module';
 import { TagModule } from './tag/tag.module';
+import { ConfigModule } from '@nestjs/config';
 
+const ENV = process.env.NODE_ENV
+console.log(process.env.NODE_ENV)
+console.log(ENV)
+console.log(!ENV ? '.env-dev': `.env-${ENV}`)
 @Module({
-  imports: [AuthModule, UsersModule, RecognitionModule, CompanyModule, TagModule, TypeOrmModule.forRoot()],
+  imports: [ ConfigModule.forRoot({
+    envFilePath: !ENV ? '.env-dev': `.env-${ENV}`
+  }),
+  AuthModule, UsersModule, RecognitionModule, CompanyModule, TagModule, TypeOrmModule.forRoot({
+    type: "postgres",
+    host: "localhost",
+    port: 5432,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    entities: ["dist/**/*.entity{.ts,.js}"],
+    synchronize: true,
+    logging: "all"
+ }),],
   controllers: [AppController],
   providers: [AppService],
 
